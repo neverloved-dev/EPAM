87f789976271686434fc6b3a22692e264d974626
Andjic Djordje <djordjeandjic99@gmail.com>
2023-10-15 12:17:03 +0200

    clean up the repository

---
+++ Exception Handling/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for(int i  = 0; i < args.Length; i++)
+            {
+                if (args[i][0] == ' ') throw new ArgumentNullException("The string is empty");
+                Console.WriteLine(args[i][0]);
+                
+            }
+        }
+    }
+}
---
+++ Exception Handling/Task2/NumberParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            int num = 0;
+            int iterationLen = stringValue.Length;
+            try
+            {
+                for (int i = 0; i < iterationLen; i++)
+                {
+                    num = num * 10 + (stringValue[i] - 48);
+                }
+
+            }
+            catch(ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+                num = 0;
+            }
+            catch(OverflowException exception)
+            {
+                Console.WriteLine($"The {exception.Data} is too big to handle");
+                num = 0;
+            }
+            catch(FormatException exception)
+            {
+                Console.WriteLine($"{exception.Message} for {exception.Data}");
+                num = 0;
+            }
+            return num;
+        }
+    }
+}
