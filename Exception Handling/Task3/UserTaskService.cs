1fb1144730ac6f249a5b58eb17370f5a67b20706
Andjic Djordje <djordjeandjic99@gmail.com>
2023-10-16 18:18:31 +0200

    Did the optional task

--- Exception Handling/Task3/UserTaskService.cs
+++ Exception Handling/Task3/UserTaskService.cs
@@ -14,23 +14,45 @@ namespace Task3
 
         public int AddTaskForUser(int userId, UserTask task)
         {
-            if (userId < 0)
-                return -1;
-
-            var user = _userDao.GetUser(userId);
-            if (user == null)
-                return -2;
-
-            var tasks = user.Tasks;
-            foreach (var t in tasks)
+            try
             {
-                if (string.Equals(task.Description, t.Description, StringComparison.OrdinalIgnoreCase))
-                    return -3;
+                if (userId < 0)
+                {
+                    throw new ArgumentException("User not found: userId should be a positive value.");
+                }
+
+
+
+                var user = _userDao.GetUser(userId);
+                if (user == null)
+                    throw new ArgumentException("User not found: The specified user does not exist.");
+
+                var tasks = user.Tasks;
+                foreach (var t in tasks)
+                {
+                    if (string.Equals(task.Description, t.Description, StringComparison.OrdinalIgnoreCase))
+                        throw new InvalidOperationException("Task already exists: A task with the same description already exists for the user.");
+                }
+
+                tasks.Add(task);
+
+                return 0;
             }
-
-            tasks.Add(task);
-
-            return 0;
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -3;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -2;
+            }
         }
     }
 }
