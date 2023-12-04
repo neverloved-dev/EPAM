7c7907cb568f638ce29f8a51d772e5aed3f339b1
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-04 11:39:16 +0100

    Small change in python script

--- SQL Task/api_call.py
+++ SQL Task/api_call.py
@@ -1,6 +1,7 @@
 import requests
 import pyodbc
 import sys
+from urllib.parse import quote
 
 access_key = "8a8ee49aab31bb3ae779ae43fd9219e2"
 url = "https://positionstack.com/v1/forward"
@@ -23,13 +24,16 @@ def insert_data(latitude: float, longitude: float):
 
 def get_data(address: str):
     try:
-        params = {"access_key": access_key, "query": address}
-        response = requests.get(url, params=params)
+        encoded_address = quote(address)  # Encode address for URL
+        url = f"{url}?access_key={access_key}&query={encoded_address}"
+        
+        response = requests.get(url)
         data = response.json()
+        
         if 'data' in data and len(data['data']) > 0:
             latitude = data['data'][0]['latitude']
             longitude = data['data'][0]['longitude']
-            insert_data(latitude, longitude)
+            insert_data(latitude, longitude, address)
         else:
             print("No location data found for the given address.")
     except requests.RequestException as e:
