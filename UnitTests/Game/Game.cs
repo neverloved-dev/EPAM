c5dbae41b8093a7d69f40cc9ebdba57d07ce7a65
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-21 11:17:33 +0100

    Added game logic

---
+++ UnitTests/Game/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Game
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += SumOfPinsInFrame(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int SumOfPinsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+    }
+
+}
