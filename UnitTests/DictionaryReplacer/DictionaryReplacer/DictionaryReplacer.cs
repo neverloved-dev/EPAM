50bf83678d318fac3416cc9fe3e98b63af736fca
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-22 12:00:49 +0100

    Add tests and logic

---
+++ UnitTests/DictionaryReplacer/DictionaryReplacer/DictionaryReplacer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DictionaryReplacerNamespace
+{
+    public class DictionaryReplacer
+    {
+        public static string ReplaceVariables(string input, Dictionary<string, string> dict)
+        {
+            if (string.IsNullOrEmpty(input) || dict.Count == 0)
+                return input;
+
+            var regex = new Regex(@"\$\w+\$");
+
+            return regex.Replace(input, match =>
+            {
+                string key = match.Value.Trim('$');
+                return dict.ContainsKey(key) ? dict[key] : match.Value;
+            });
+        }
+    }
+}
---
+++ UnitTests/DictionaryReplacer/DictionaryReplacerTests/DictionaryReplacer.cs
@@ -0,0 +1,26 @@
+using DictionaryReplacerNamespace;
+namespace DictionaryReplacerTests
+{
+    public class DictionaryReplacer
+    {
+        [Theory]
+        [InlineData("", new string[0], "")]
+        [InlineData("$temp$", new string[] { "temp", "temporary" }, "temporary")]
+        [InlineData("$temp$ here comes the name $name$", new string[] { "temp", "temporary", "name", "John Doe" }, "temporary here comes the name John Doe")]
+        public void ReplaceVariables_WithDictionary_ReplacesVariablesCorrectly(string input, string[] dictValues, string expectedOutput)
+        {
+            // Arrange
+            var dict = new Dictionary<string, string>();
+            for (int i = 0; i < dictValues.Length; i += 2)
+            {
+                dict[dictValues[i]] = dictValues[i + 1];
+            }
+
+            // Act
+            var result = DictionaryReplacer.ReplaceVariables(input, dict);
+
+            // Assert
+            Assert.Equal(expectedOutput, result);
+        }
+    }
+}
