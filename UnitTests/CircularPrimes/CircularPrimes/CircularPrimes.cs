7b2c0b71d346fc2ff2be91bdecff01ceb764d3d0
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-22 11:18:46 +0100

    Add tests and logic

--- UnitTests/CircularPrimes/CircularPrimes/CircularPrimes.cs
+++ UnitTests/CircularPrimes/CircularPrimes/CircularPrimes.cs
@@ -1,9 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CircularPrimesNamespace
 {
     public class CircularPrimes
     {
+        // Function to check if a number is prime
+        private static bool IsPrime(int num)
+        {
+            if (num <= 1)
+                return false;
 
+            if (num == 2)
+                return true;
+
+            if (num % 2 == 0)
+                return false;
+
+            for (int i = 3; i <= Math.Sqrt(num); i += 2)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        // Function to generate all rotations of a number
+        private static List<int> GenerateRotations(int num)
+        {
+            List<int> rotations = new List<int>();
+            string numStr = num.ToString();
+
+            for (int i = 0; i < numStr.Length; i++)
+            {
+                rotations.Add(int.Parse(numStr));
+                numStr = numStr[numStr.Length - 1] + numStr.Substring(0, numStr.Length - 1);
+            }
+
+            return rotations;
+        }
+
+        // Function to count circular primes below N
+        public static int CountCircularPrimesBelowN(int N)
+        {
+            int count = 0;
+
+            for (int i = 2; i < N; i++)
+            {
+                List<int> rotations = GenerateRotations(i);
+                bool isCircularPrime = true;
+
+                foreach (int rotation in rotations)
+                {
+                    if (!IsPrime(rotation))
+                    {
+                        isCircularPrime = false;
+                        break;
+                    }
+                }
+
+                if (isCircularPrime)
+                    count++;
+            }
+
+            return count;
+        }
     }
 }
+
--- UnitTests/CircularPrimes/CircularPrimesTests/CircularPrimeTests.cs
+++ UnitTests/CircularPrimes/CircularPrimesTests/CircularPrimeTests.cs
@@ -1,11 +1,23 @@
+using CircularPrimesNamespace;
 namespace CircularPrimesTests
 {
     public class CircularPrimeTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 0)] // There are 0 circular primes below 1
+        [InlineData(10, 4)] // There are 4 circular primes below 10
+        [InlineData(100, 13)] // There are 13 circular primes below 100
+        [InlineData(1000, 25)] // There are 25 circular primes below 1000
+        [InlineData(10000, 33)] // There are 33 circular primes below 10000
+        [InlineData(100000, 43)] // There are 43 circular primes below 100000
+        [InlineData(1000000, 55)] // There are 55 circular primes below 1000000
+        public void CountCircularPrimesBelowN_ReturnsCorrectCount(int N, int expectedCount)
         {
+            // Act
+            int circularPrimesCount = CircularPrimes.CountCircularPrimesBelowN(N);
 
+            // Assert
+            Assert.Equal(expectedCount, circularPrimesCount);
         }
     }
 }
