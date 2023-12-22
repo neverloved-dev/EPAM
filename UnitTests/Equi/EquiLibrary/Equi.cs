72dbbcb83489b20c4324f65130296383be140a8c
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-22 15:58:14 +0100

    Added Run code

--- UnitTests/Equi/EquiLibrary/Equi.cs
+++ UnitTests/Equi/EquiLibrary/Equi.cs
@@ -2,5 +2,24 @@
 
 public class Equi
 {
+  public static int Solution(int[] A)
+        {
+            if (A == null || A.Length == 0)
+                return -1;
 
+            long totalSum = A.Sum();
+            long leftSum = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                totalSum -= A[i];
+
+                if (leftSum == totalSum)
+                    return i;
+
+                leftSum += A[i];
+            }
+
+            return -1;
+        }
 }
