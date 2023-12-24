b91cd99d2196380ce2e70bc227cde791eace673b
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-24 16:52:48 +0100

    Add tests

--- UnitTests/Labyrinth/LabyrinthTests/LabyrinthTests.cs
+++ UnitTests/Labyrinth/LabyrinthTests/LabyrinthTests.cs
@@ -1,11 +1,79 @@
+using LabyrinthNamespace;
+
 namespace LabyrinthTests
 {
     public class LabyrinthTests
     {
         [Fact]
-        public void Test1()
+        public void TestFindExit_PathFound()
         {
+            char[,] labyrinth = {
+            {'*', '*', '*', '*', '*', '*'},
+            {'_', '*', '_', '_', '_', '*'},
+            {'*', 's', '_', '*', '_', '*'},
+            {'*', '*', '*', '*', '_', '*'},
+            {'_', '_', 'e', '_', '_', '*'},
+            {'_', '*', '*', '*', '_', '*'}
+        };
 
+            Labyrinth solver = new Labyrinth(labyrinth);
+            bool pathFound = solver.FindExit();
+
+            Assert.True(pathFound);
+        }
+
+        [Fact]
+        public void TestFindExit_NoPathFound()
+        {
+            char[,] labyrinth = {
+            {'*', '*', '*', '*', '*', '*'},
+            {'_', '*', '_', '_', '_', '*'},
+            {'*', 's', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '_', '*'},
+            {'_', '_', 'e', '_', '_', '*'},
+            {'_', '*', '*', '*', '_', '*'}
+        };
+
+            Labyrinth solver = new Labyrinth(labyrinth);
+            bool pathFound = solver.FindExit();
+
+            Assert.False(pathFound);
+        }
+
+        [Fact]
+        public void TestFindExit_StartPointNotFound()
+        {
+            char[,] labyrinth = {
+            {'*', '*', '*', '*', '*', '*'},
+            {'_', '*', '_', '_', '_', '*'},
+            {'*', '*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '_', '*'},
+            {'_', '_', 'e', '_', '_', '*'},
+            {'_', '*', '*', '*', '_', '*'}
+        };
+
+            Labyrinth solver = new Labyrinth(labyrinth);
+            bool pathFound = solver.FindExit();
+
+            Assert.False(pathFound);
+        }
+
+        [Fact]
+        public void TestFindExit_EmptyLabyrinth()
+        {
+            char[,] labyrinth = {
+            {'*', '*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '*', '*'}
+        };
+
+            Labyrinth solver = new Labyrinth(labyrinth);
+            bool pathFound = solver.FindExit();
+
+            Assert.False(pathFound);
         }
     }
 }
