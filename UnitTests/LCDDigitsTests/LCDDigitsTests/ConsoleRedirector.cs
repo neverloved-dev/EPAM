88d93cec9817b3564a12470602e6c60ae2cd9152
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-24 18:43:02 +0100

    Add tests logic

---
+++ UnitTests/LCDDigitsTests/LCDDigitsTests/ConsoleRedirector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCDDigitsTests
+{
+    public class ConsoleRedirector : IDisposable
+    {
+        private readonly StringWriter stringWriter;
+        private readonly TextWriter originalOutput;
+
+        public ConsoleRedirector()
+        {
+            stringWriter = new StringWriter();
+            originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+        }
+
+        public string GetOutput()
+        {
+            return stringWriter.ToString();
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(originalOutput);
+            stringWriter.Dispose();
+        }
+    }
+}
--- UnitTests/LCDDigitsTests/LCDDigitsTests/UnitTest1.cs
+++ UnitTests/LCDDigitsTests/LCDDigitsTests/UnitTest1.cs
@@ -1,11 +1,99 @@
 namespace LCDDigitsTests
 {
-    public class UnitTest1
+    public class LCDDigitsTests
     {
         [Fact]
-        public void Test1()
+        public void TestPrintLCDDigits_SingleDigit()
         {
+            LCDDigits lcdDigits = new LCDDigits();
+            int number = 7;
 
+            string[] expectedOutput = {
+            "._. ",
+            "..| ",
+            "..| "
+        };
+
+            AssertPrintLCDDigits(lcdDigits, number, expectedOutput);
         }
+
+        [Fact]
+        public void TestPrintLCDDigits_MultipleDigits()
+        {
+            LCDDigits lcdDigits = new LCDDigits();
+            int number = 12345;
+
+            string[] expectedOutput = {
+            "._. ._. ._. ._. ._. ",
+            "|_| ..| |_. |_. ..| ",
+            "..| ..| |_| ._| ..| "
+        };
+
+            AssertPrintLCDDigits(lcdDigits, number, expectedOutput);
+        }
+
+        [Fact]
+        public void TestPrintLCDDigits_Zero()
+        {
+            LCDDigits lcdDigits = new LCDDigits();
+            int number = 0;
+
+            string[] expectedOutput = {
+            "._. ",
+            "|.| ",
+            "|_| "
+        };
+
+            AssertPrintLCDDigits(lcdDigits, number, expectedOutput);
+        }
+
+        [Fact]
+        public void TestPrintLCDDigits_NegativeNumber()
+        {
+            LCDDigits lcdDigits = new LCDDigits();
+            int number = -8;
+
+            string[] expectedOutput = {
+            "... ",
+            "..| ",
+            "..| "
+        };
+
+            AssertPrintLCDDigits(lcdDigits, number, expectedOutput);
+        }
+
+        [Fact]
+        public void TestPrintLCDDigits_MaxInteger()
+        {
+            LCDDigits lcdDigits = new LCDDigits();
+            int number = int.MaxValue;
+
+            string[] expectedOutput = {
+            "._. ._. ._. ._. ._. ._. ._. ._. ._. ._. ",
+            "|.| ..| ..| |.| |.| |.| |.| |.| |.| |.| ",
+            "|_| ..| |_| |_| |_| |_| |_| |_| |_| |_| "
+        };
+
+            AssertPrintLCDDigits(lcdDigits, number, expectedOutput);
+        }
+
+        private void AssertPrintLCDDigits(LCDDigits lcdDigits, int number, string[] expectedOutput)
+        {
+            using (ConsoleRedirector consoleRedirector = new ConsoleRedirector())
+            {
+                lcdDigits.PrintLCDDigits(number);
+
+                string[] actualOutput = consoleRedirector.GetOutput().Trim().Split('\n');
+
+                Assert.Equal(expectedOutput.Length, actualOutput.Length);
+
+                for (int i = 0; i < expectedOutput.Length; i++)
+                {
+                    Assert.Equal(expectedOutput[i].Trim(), actualOutput[i].Trim());
+                }
+            }
+        }
+
+      
     }
 }
