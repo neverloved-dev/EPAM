b01fa118098e45b9a821f98b7d6a88c466066ac1
Andjic Djordje <djordjeandjic99@gmail.com>
2023-12-25 13:49:54 +0100

    Initialize empty project

---
+++ EPAM_MVC/EPAM_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EPAM_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
