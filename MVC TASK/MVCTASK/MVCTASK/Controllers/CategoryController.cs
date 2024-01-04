f7ac595353c3271fd83dd79d5a98ad2f1b5de010
Andjic Djordje <djordjeandjic99@gmail.com>
2024-01-04 12:51:29 +0100

    INIT working on the frontend
    1. Added Categories.cshtml file and set up the view for the categories that will come in later
    
    2.Add LisAll.cshtml for products to list all products
    
    3. Add CategoryController.cs to handle categories logic
    
    4. Add ProductController.cs to handle products logic
    
    5. Edited _Layout.cshtml file so the user can navigate between the pages

---
+++ MVC TASK/MVCTASK/MVCTASK/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using MVCTASK.Models;
+
+
+namespace MVCTASK.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ILogger<CategoryController> _logger;
+
+    public CategoryController(ILogger<CategoryController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Categories()
+    {
+        return View();
+    }
+}
---
+++ MVC TASK/MVCTASK/MVCTASK/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTASK.Controllers;
+
+public class ProductController : Controller
+{
+    // GET
+    public IActionResult ListAll()
+    {
+        return View();
+    }
+}
