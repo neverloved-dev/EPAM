dcb4b9cca550d6ea7e7339973f3581928b6a3a12
Andjic Djordje <djordjeandjic99@gmail.com>
2024-01-04 13:42:17 +0100

    Add Product.cs and Category.cs as models

---
+++ MVC TASK/MVCTASK/MVCTASK/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace MVCTASK.Models;
+
+public class Category
+{
+    public int Id { get; private set; }
+    public string Name;
+}
---
+++ MVC TASK/MVCTASK/MVCTASK/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCTASK.Models;
+
+public class Product
+{
+    public int Id { get; private set; }
+    
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+}
