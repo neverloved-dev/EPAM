dfccfca1386999fd006c0bf9e1da06dc432c03d6
Andjic Djordje <djordjeandjic99@gmail.com>
2024-01-08 16:36:06 +0100

    ADD ProductService.cs
    Add service logic for Products

---
+++ WebTask/Services/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using WebTask.Models;
+
+namespace WebTask.Services;
+
+public class ProductService:IGenericService<Product>
+{
+    private DataContext _context;
+
+    public ProductService(DataContext context)
+    {
+        _context = context;
+    }
+    public void Add(Product entity)
+    {
+        _context.Products.Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var productToDelete = _context.Products.Find(id);
+        if (productToDelete == null)
+        {
+            throw new NullReferenceException("The object could not be found!");
+        }
+
+        _context.Products.Remove(productToDelete);
+        _context.SaveChanges();
+    }
+
+    public void Update(Product entity)
+    {
+        _context.Products.Update(entity);
+        _context.SaveChanges();
+    }
+
+    public List<Product> GetAll()
+    {
+        return _context.Products.ToList();
+    }
+
+    public Product Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
