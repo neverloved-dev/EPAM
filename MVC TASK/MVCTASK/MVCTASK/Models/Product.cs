57957d57f8ec5c52fcfb4c514a3c26df095a1621
Andjic Djordje <djordjeandjic99@gmail.com>
2024-01-08 14:56:00 +0100

    ADD server side validation for Product.cs

--- MVC TASK/MVCTASK/MVCTASK/Models/Product.cs
+++ MVC TASK/MVCTASK/MVCTASK/Models/Product.cs
@@ -1,10 +1,16 @@
-﻿namespace MVCTASK.Models;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCTASK.Models;
 
 public class Product
 {
     public int Id { get; private set; }
-    
+    [Required(ErrorMessage = "Please enter a product name")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Please enter a product price!")]
+    [Range(0.01, Double.MaxValue, ErrorMessage = "Price must be greater than zero")]
     public decimal Price { get; set; }
+    [Required(ErrorMessage = "You must select a category")]
     public int CategoryId { get; set; }
 }
