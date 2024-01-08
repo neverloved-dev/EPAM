9233d540d05afc2174f4a851907aa50b86d27fe8
Andjic Djordje <djordjeandjic99@gmail.com>
2024-01-08 16:15:40 +0100

    Add IGenericService.cs

---
+++ WebTask/Services/IGenericService.cs
@@ -0,0 +1,10 @@
+﻿namespace WebTask.Services;
+
+public interface IGenericService<T>
+{
+    public void Add(T entity);
+    public void Delete(int id);
+    public void Update(T entity);
+    public List<T> GetAll();
+    public T Get(int id);
+}
