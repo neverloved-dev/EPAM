a6736d2e51b1e2b1ec301e98b11f113d737068d5
Andjic Djordje <djordjeandjic99@gmail.com>
2024-01-08 14:05:36 +0100

    ADD new config and logic
    Added following things:
    1. Changed configurations to add max number of products fetched
    2. Added New.cshtml which is a form for new product
    3. Updated ProductController.cs to fetch the number of products based on configurations and also add get request for the new form

--- MVC TASK/MVCTASK/MVCTASK/Controllers/ProductController.cs
+++ MVC TASK/MVCTASK/MVCTASK/Controllers/ProductController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVCTASK.Models;
 
 namespace MVCTASK.Controllers;
 
 public class ProductController : Controller
 {
+    private readonly IConfiguration _configuration;
+    private readonly DataContext _context;
+    public ProductController(IConfiguration configuration, DataContext context)
+    {
+        _configuration = configuration;
+        _context = context;
+    }
+    
     // GET
     public IActionResult ListAll()
     {
-        return View();
+        int productCount = _configuration.GetValue<int>("MaxProductCount");
+        List<Product> products;
+        if (productCount > 0)
+        {
+          products =  _context.Products.Take(productCount).ToList();   
+        }
+        else
+        {
+            products = _context.Products.ToList();
+        }
+        return View(products);
     }
+
+    public IActionResult New()
+    {
+        ViewBag.Categories = GetCategoriesSelectList();
+        return View(new Product());
+    }
+
+    private SelectList GetCategoriesSelectList(int selectedCategory = 0)
+    {
+        List<Category> categories = _context.Categories.ToList();
+        return new SelectList(categories, "Id", "Name", selectedCategory);
+    }
+    
 }
