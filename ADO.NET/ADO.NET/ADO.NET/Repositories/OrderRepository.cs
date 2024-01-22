21b3346ffa0d6b174754f2732ad654c2690f7cf2
Andjic Djordje <djordjeandjic99@gmail.com>
2024-01-22 15:18:53 +0100

    Add order logic

--- ADO.NET/ADO.NET/ADO.NET/Repositories/OrderRepository.cs
+++ ADO.NET/ADO.NET/ADO.NET/Repositories/OrderRepository.cs
@@ -1,31 +1,107 @@
 ﻿using ADO.NET.Models;
+using Microsoft.Data.SqlClient;
 
 namespace ADO.NET;
 
-public class OrderRepository:IGenericRepository<Order>
+public class OrderRepository
 {
-    public void CreateT(Order entity)
+    private readonly string connectionString;
+
+    public OrderRepository(string connectionString)
     {
-        throw new NotImplementedException();
+        this.connectionString = connectionString;
     }
 
-    public List<Order> GetAll()
+    public void InsertOrder(Order order)
     {
-        throw new NotImplementedException();
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "INSERT INTO Orders (Status, CreatedDate, UpdatedDate, ProductId) " +
+                           "VALUES (@Status, @CreatedDate, @UpdatedDate, @ProductId)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Status", order.Status.ToString());
+                command.Parameters.AddWithValue("@CreatedDate", order.CreatedDate);
+                command.Parameters.AddWithValue("@UpdatedDate", order.UpdatedDate);
+                command.Parameters.AddWithValue("@ProductId", order.ProductId);
+
+                command.ExecuteNonQuery();
+            }
+        }
     }
 
-    public Order Get(int id)
+    public Order GetOrderById(int orderId)
     {
-        throw new NotImplementedException();
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT ID, Status, CreatedDate, UpdatedDate, ProductId " +
+                           "FROM Orders WHERE ID = @ID";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@ID", orderId);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Order(
+                            (int)reader["ID"],
+                            Enum.Parse<Status>(reader["Status"].ToString()),
+                            (DateTime)reader["CreatedDate"],
+                            (DateTime)reader["UpdatedDate"],
+                            (int)reader["ProductId"]
+                        );
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 
-    public void Delete(int id)
+    public void UpdateOrder(Order order)
     {
-        throw new NotImplementedException();
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "UPDATE Orders SET Status = @Status, " +
+                           "CreatedDate = @CreatedDate, UpdatedDate = @UpdatedDate, " +
+                           "ProductId = @ProductId WHERE ID = @ID";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@ID", order.ID);
+                command.Parameters.AddWithValue("@Status", order.Status.ToString());
+                command.Parameters.AddWithValue("@CreatedDate", order.CreatedDate);
+                command.Parameters.AddWithValue("@UpdatedDate", order.UpdatedDate);
+                command.Parameters.AddWithValue("@ProductId", order.ProductId);
+
+                command.ExecuteNonQuery();
+            }
+        }
     }
 
-    public Order Update(Order entity, int id)
+    public void DeleteOrder(int orderId)
     {
-        throw new NotImplementedException();
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "DELETE FROM Orders WHERE ID = @ID";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@ID", orderId);
+
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
