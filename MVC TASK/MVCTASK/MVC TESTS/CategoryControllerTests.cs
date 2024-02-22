e27c719c511b5f4f5115506e71832ccefdb3d54a
neverloved-dev <djordjeandjic99@gmail.com>
2024-02-22 19:54:35 +0100

    write some test ideas

---
+++ MVC TASK/MVCTASK/MVC TESTS/CategoryControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MVCTASK.Controllers;
+using MVCTASK.Models;
+
+namespace MVC_TESTS
+{
+
+    public class CategoryControllerTests
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private DataContext _context = new DataContext(options=>options.UseInMemoryDatabase("InMemoryDb"));
+        public CategoryControllerTests() 
+        {
+            CategoryController categoryController = new CategoryController(_logger,_context);
+        }
+
+        [Fact]
+        public void CategoryControllerGetCategoriesShouldReturnOkResult()
+        {
+
+        }
+    }
+}
