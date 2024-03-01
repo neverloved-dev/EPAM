412933e4a3ed40376bcd6182732161d18780ccf5
neverloved-dev <djordjeandjic99@gmail.com>
2024-03-01 14:00:32 +0100

    Define CategoryControllerTests.cs methods

--- WebTaskTests/CategoryControllerTests.cs
+++ WebTaskTests/CategoryControllerTests.cs
@@ -3,9 +3,33 @@ namespace WebTaskTests
     public class CategoryControllerTests
     {
         [Fact]
-        public void Test1()
+        public void GetCategoriesReturns200WithListOfCategories()
         {
 
         }
+
+        [Fact]
+        public void PutCategoryReturns200()
+        {
+
+        }
+
+        [Fact]
+        public void PostCategoryReturns200WithNewCategoryInsideList()
+        {
+
+        }
+
+        [Fact]
+        public void ReturnsSingleCategoryWith200Response()
+        {
+
+        }
+
+        [Fact]
+        public void DeletesSingleCategoryWith200Response()
+        {
+
+        
     }
 }
