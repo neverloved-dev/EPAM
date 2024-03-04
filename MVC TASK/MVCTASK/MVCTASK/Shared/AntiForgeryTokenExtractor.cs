d9d76c7d44bda85c70bf81ab7739da3815b5c151
neverloved-dev <djordjeandjic99@gmail.com>
2024-03-04 09:04:29 +0100

    Add method to extract cookie value

--- MVC TASK/MVCTASK/MVCTASK/Shared/AntiForgeryTokenExtractor.cs
+++ MVC TASK/MVCTASK/MVCTASK/Shared/AntiForgeryTokenExtractor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Azure;
+using Microsoft.Net.Http.Headers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,16 @@ namespace MVCTASK.Shared
         public static string AntiForgeryFieldName { get; } = "AntiForgeryTokenField";
         public static string AntiForgeryCookieName { get; } = "AntiForgeryTokenCookie";
 
-
+        public static string ExtractAntiForgeryCookieValue(HttpResponseMessage httpResponse)
+        {
+            var antiForgeryCookie = httpResponse.Headers.GetValues("Set-Cookie")
+                .FirstOrDefault(x=>x.Contains(AntiForgeryCookieName));
+            if (antiForgeryCookie is null)
+            {
+                throw new ArgumentException($"Cookie '{AntiForgeryCookieName}' not found in HTTP response", nameof(httpResponse));
+            }
+            var antiForgeryCookieValue = SetCookieHeaderValue.Parse(antiForgeryCookie).Value.ToString();
+            return antiForgeryCookieValue;
+        }
     }
 }
