e8826e3aa8cf7329e29616f30663ea6e9ecd4beb
neverloved-dev <djordjeandjic99@gmail.com>
2024-03-04 09:06:59 +0100

    Add method to extract the token from the cookie

--- MVC TASK/MVCTASK/MVCTASK/Shared/AntiForgeryTokenExtractor.cs
+++ MVC TASK/MVCTASK/MVCTASK/Shared/AntiForgeryTokenExtractor.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MVCTASK.Shared
@@ -24,5 +25,16 @@ namespace MVCTASK.Shared
             var antiForgeryCookieValue = SetCookieHeaderValue.Parse(antiForgeryCookie).Value.ToString();
             return antiForgeryCookieValue;
         }
+
+        private static string ExtractAntiForgeryToken(string htmlBody)
+        {
+            var requestVerificationTokenMatch =Regex.Match(htmlBody, $@"\<input name=""{AntiForgeryFieldName}"" type=""hidden"" value=""([^""]+)"" \/\>");
+            if (requestVerificationTokenMatch.Success)
+                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
+            throw new ArgumentException($"Anti forgery token '{AntiForgeryFieldName}' not found in HTML", nameof(htmlBody));
+        }
+
+        // 
     }
+    
 }
